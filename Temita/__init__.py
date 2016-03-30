86c30f754cb8ab0fb833d047221ac8d304b42eff
Admin <Admin@Aitor>
2016-03-30 09:34:28 +0200

    layout.xml y pages.xml services openacademy

--- Temita/__init__.py
+++ Temita/__init__.py
@@ -1,1 +1,1 @@
-
+from . import mymodule
--- Temita/__openerp__.py
+++ Temita/__openerp__.py
@@ -1,12 +1,13 @@
 {
   'name':'theme_Temita',
-  'description': 'A description for your theme.',
-  'version':'1.0',
+  'description': '1.1',
+  'version':'1.1',
   'author':'Your name',
 
   'data': [
   'views/layout.xml',
-  'views/pages.xml'
+  'views/pages.xml',
+  'views/assets.xml'
   ],
   'category': 'Theme/Creative',
   'depends': ['website', 'website_blog', 'sale'],
